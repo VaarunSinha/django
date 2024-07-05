12705e257eed2d900b3de6292aac2e1fe829a2c9
Vaarun Sinha <85062344+VaarunSinha@users.noreply.github.com>
2024-07-05 17:35:03 +0530

    Fixed #35083 -- Made method_decorator work with async functions.
    Co-authored-by: nessita <124304+nessita@users.noreply.github.com>

--- django/utils/decorators.py
+++ django/utils/decorators.py
@@ -37,15 +37,27 @@ def _multi_decorate(decorators, method):
     else:
         decorators = [decorators]
 
-    def _wrapper(self, *args, **kwargs):
-        # bound_method has the signature that 'decorator' expects i.e. no
-        # 'self' argument, but it's a closure over self so it can call
-        # 'func'. Also, wrap method.__get__() in a function because new
-        # attributes can't be set on bound method objects, only on functions.
-        bound_method = wraps(method)(partial(method.__get__(self, type(self))))
-        for dec in decorators:
-            bound_method = dec(bound_method)
-        return bound_method(*args, **kwargs)
+    # Below, for both the async and sync variants:
+    # bound_method has the signature that 'decorator' expects i.e. no 'self'
+    # argument, but it's a closure over self so it can call 'func'. Also, wrap
+    # method.__get__() in a function because new attributes can't be set on
+    # bound method objects, only on functions.
+
+    if iscoroutinefunction(method):
+
+        async def _wrapper(self, *args, **kwargs):
+            bound_method = wraps(method)(partial(method.__get__(self, type(self))))
+            for dec in decorators:
+                bound_method = dec(bound_method)
+            return await bound_method(*args, **kwargs)
+
+    else:
+
+        def _wrapper(self, *args, **kwargs):
+            bound_method = wraps(method)(partial(method.__get__(self, type(self))))
+            for dec in decorators:
+                bound_method = dec(bound_method)
+            return bound_method(*args, **kwargs)
 
     # Copy any attributes that a decorator adds to the function it decorates.
     for dec in decorators:
